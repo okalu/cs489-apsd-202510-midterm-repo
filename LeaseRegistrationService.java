68fb1341dda5ccc28073d44aa0be9df4f2d8a0ed
nhienlq <quangnhienxstkk26@gmail.com>
2025-10-07 10:31:52 -0500

    add new feature

---
+++ LeaseRegistrationService.java
@@ -0,0 +1,10 @@
+package cs489-apsd-202510-midterm-repo;
+
+public class LeaseRegistrationService {
+    public void createNewLease() {
+
+System.out.println("Hello, New Lease created!");
+
+}
+    
+}
