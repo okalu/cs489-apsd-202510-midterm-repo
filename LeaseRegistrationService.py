1dfc6a01ae3c4e978d48b296c58aac649c37b930
hossamhsn77 <hossam.h@aicomai.ai>
2025-10-07 10:12:15 -0500

    Add LeaseRegistrationService class for handling lease registrations

---
+++ LeaseRegistrationService.py
@@ -0,0 +1,6 @@
+class LeaseRegistrationService:
+    """Service for handling lease registrations."""
+
+    def createNewLease(self):
+        """Creates a new lease registration."""
+        print("creating new lease ..")
