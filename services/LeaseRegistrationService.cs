c6b1055c935067e7eb32ff13e19c7be2a8858571
Mohamed Hussein <mohamed.hussein.tawfick@gmail.com>
2025-10-07 10:28:15 -0500

    feat: add LeaseRegistrationService.cs with CreateNewLease()

---
+++ services/LeaseRegistrationService.cs
@@ -0,0 +1,10 @@
+namespace Leasing.Services
+{
+    public class LeaseRegistrationService
+    {
+        public void CreateNewLease()
+        {
+            Console.WriteLine("Hello, New Lease created!");
+        }
+    }
+}
