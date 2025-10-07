7aead193c410232addc43a25e052e94ab2568847
Sunil Poudel <sunil.poudel@miu.edu>
2025-10-07 10:45:20 -0500

    new lease service added

---
+++ src/main/java/edu/miu/cs/cs489/aprtlease/LeaseRegistrationService.java
@@ -0,0 +1,11 @@
+package edu.miu.cs.cs489.aprtlease;
+
+public class LeaseRegistrationService {
+
+    public void createNewLease() {
+
+        System.out.println("Hello, New Lease created!");
+
+    }
+
+}
